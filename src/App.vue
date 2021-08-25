93f6afc5a55c8a25d01ef6e03ebd9443fb88c024
Igor Prates <igoorprates@hotmail.com.br>
2021-08-24 21:25:37 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Card msg="Teste" />
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>
+
+<style lang="css">
+  @import './styles/home.css';
+</style>
